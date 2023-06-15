7759803011069f825bd4cffeba15adf6c4d387bd
Пользователь <milausik@ukr.net>
2023-06-15 12:10:20 +0300

    added basket, link in footer

--- js/main.js
+++ js/main.js
@@ -17,7 +17,22 @@
     }
   })();
 
+//index
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+        document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
 
   // index
   $(document).ready(function () {
@@ -183,5 +198,6 @@ $('.fade').slick({
   arrows: false,
   cssEase: 'linear'
 });
+
 
 
